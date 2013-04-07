bf14c30e33d862d51fe648ff3f593fd090a91906
Aleksey Porfirov <lexqt@yandex.ru>
2013-04-07 17:40:10 +0400

    Enhance templates and styles

--- aircat/aircat/settings.py
+++ aircat/aircat/settings.py
@@ -106,6 +106,14 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+#    'django.core.context_processors.debug',
+#    'django.core.context_processors.i18n',
+#    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+#    'django.core.context_processors.tz',
+)
+
 ROOT_URLCONF = 'aircat.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
