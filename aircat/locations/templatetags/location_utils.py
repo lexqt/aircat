a6d7b319108686090dcf868fba361a6545016747
Aleksey Porfirov <lexqt@yandex.ru>
2013-04-07 19:55:31 +0400

    Add Google Maps urls for locations

--- aircat/locations/templatetags/location_utils.py
+++ aircat/locations/templatetags/location_utils.py
@@ -3,6 +3,10 @@
 from __future__ import unicode_literals
 
 from django import template
+from django.utils.http import urlencode
+from django.utils.safestring import mark_safe
+
+from locations.models import Airport, City, Country
 
 
 register = template.Library()
@@ -16,3 +20,19 @@ def alpha_range(lang):
     elif lang == 'en':
         return 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     return ''
+
+
+@register.simple_tag
+def gmaps_url(location):
+    lat, lon = location.latitude, location.longitude
+    params = {
+        'll': '{0},{1}'.format(lat, lon)
+    }
+    if isinstance(location, Airport):
+        z = 15
+    elif isinstance(location, City):
+        z = 9
+    else:
+        z = 5
+    params['z'] = z
+    return mark_safe('http://maps.google.ru/?' + urlencode(params))
